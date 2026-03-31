4e7c21519ea288c50ca5c9305d3fdb6dba7f1f39
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-04-01 00:41:28 +0700

    add requests

--- backend/src/api/crud.py
+++ backend/src/api/crud.py
@@ -4,24 +4,24 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
-from backend.src.api.models import User
-from backend.src.api.schemas import UserCreate
+from auth import get_password_hash
+from models import User
+from schemas import UserCreate
 
 
 async def get_user_by_email(db: AsyncSession, email: str):
     result = await db.execute(select(User).where(User.email == email))
-    return result if result else None
+    return result.scalar_one_or_none()
 
 async def get_user_by_username(db: AsyncSession, username: str):
     result = await db.execute(select(User).where(User.username == username))
-    return result if result else None
+    return result.scalar_one_or_none()
 
 async def get_user_by_id(db: AsyncSession, user_id: int) -> User | None:
     result = await db.execute(select(User).where(User.id == user_id))
     return result if result else None
 
 async def create_user(db: AsyncSession, user: UserCreate, code, expires):
-    from backend.src.api.auth import get_password_hash
     hashed_password = get_password_hash(user.password)
     db_user = User(
         email=user.email,
@@ -39,6 +39,7 @@ async def create_user(db: AsyncSession, user: UserCreate, code, expires):
 
 async def verify_user_email(db: AsyncSession, email: str, code: str) -> bool:
     user = await get_user_by_email(db, email)
+    # print(type(user.verification_code),type(code))
     if not user or user.verification_code != code:
         return False
     if user.code_expires_at and user.code_expires_at < datetime.utcnow():
